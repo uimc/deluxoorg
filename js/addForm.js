eb2ddf45ac83f00c0d3e65ef642d355c56defd16
Michael <20736150+m3c@users.noreply.github.com>
2025-02-04 13:52:09 -0600

    Update addForm.js
    CORS fix

--- js/addForm.js
+++ js/addForm.js
@@ -1,11 +1,14 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzKFoAIUWIOGJPl9CuUD3Ac_gQUnjzHywF-ugpbb1WjZLWLbgYFEz0q3xydy795tREiNg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzKFoAIUWIOGJPl9CuUD3Ac_gQUnjzHywF-ugpbb1WjZLWLbgYFEz0q3xydy795tREiNg/exec';
 
-const form = document.forms['addForm']
+const form = document.forms['addForm'];
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  //.then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  e.preventDefault();
+  fetch(scriptURL, { 
+    method: 'POST',
+    mode: 'no-cors',  // Fix CORS issue (Browser will not block it)
+    body: new FormData(form)
+  })
+  .then(() => { window.location.reload(); }) // Refresh after submission
+  .catch(error => console.error('Error!', error.message));
+});
